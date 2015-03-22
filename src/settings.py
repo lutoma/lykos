1726eea97d79925fa95e2fba920484eb855782ac
Vgr E.Barry <vgr255@live.ca>
2015-03-21 20:55:02 -0400

    Fix arguments in super() call

--- src/settings.py
+++ src/settings.py
@@ -16,53 +16,53 @@ class SettingsDict(dict):
 
 class SettingsAPI:
     def __init__(self):
-        super().__setattr__(self, "_settings", SettingsDict())
-        super().__setattr__(self, "_non_settings", [])
+        super().__setattr__("_settings", SettingsDict())
+        super().__setattr__("_non_settings", [])
 
     def __getattribute__(self, item):
         if not hasattr(defaultsettings, item.upper()):
             raise AttributeError
-        if item in super().__getattribute__(self, "_settings"):
-            return super().__getattribute__(self, "_settings")[item]
+        if item in super().__getattribute__("_settings"):
+            return super().__getattribute__("_settings")[item]
         return getattr(botconfig, item.upper(), getattr(defaultsettings, item.upper()))
 
     def __getattr__(self, item):
         if hasattr(defaultsettings, item):
             return getattr(defaultsettings, item)
         if item == "clear":
-            return super().__getattribute__(self, "clear")
+            return super().__getattribute__("clear")
         raise AttributeError("no such setting or function: " + item)
 
     def __setattr__(self, item, value):
         if not hasattr(defaultsettings, item.upper()):
             setattr(defaultsettings, item.upper(), value)
-            super().__getattribute__(self, "_non_settings").append(item.upper())
-        elif item.upper() in super().__getattribute__(self, "_non_settings"):
+            super().__getattribute__("_non_settings").append(item.upper())
+        elif item.upper() in super().__getattribute__("_non_settings"):
             setattr(defaultsettings, item.upper(), value)
         else:
-            super().__getattribute__(self, "_settings")[item] = value
+            super().__getattribute__("_settings")[item] = value
 
     def __delattr__(self, item):
-        if item.upper() in super().__getattribute__(self, "_non_settings"):
+        if item.upper() in super().__getattribute__("_non_settings"):
             delattr(defaultsettings, item.upper())
-            del super().__getattribute__(self, "_non_settings")[item]
-        if item in super().__getattribute__(self, "_settings"):
-            del super().__getattribute__(self, "_settings")[item]
+            del super().__getattribute__("_non_settings")[item]
+        if item in super().__getattribute__("_settings"):
+            del super().__getattribute__("_settings")[item]
 
     def __getitem__(self, item):
-        return super().__getattribute__(self, "_settings").__getitem__(item)
+        return super().__getattribute__("_settings").__getitem__(item)
 
     def __setitem__(self, item, value):
-        super().__getattribute__(self, "_settings").__setitem__(item, value)
+        super().__getattribute__("_settings").__setitem__(item, value)
 
     def __delitem__(self, item):
-        super().__getattribute__(self, "_settings").__delitem__(item)
+        super().__getattribute__("_settings").__delitem__(item)
 
     def __contains__(self, item):
-        return item.upper() in super().__getattribute__(self, "_settings")
+        return item.upper() in super().__getattribute__("_settings")
 
     def __len__(self):
-        return super().__getattribute__(self, "_settings").__len__()
+        return super().__getattribute__("_settings").__len__()
 
     def clear(self):
-        super().__getattribute__(self, "_settings").clear()
+        super().__getattribute__("_settings").clear()
